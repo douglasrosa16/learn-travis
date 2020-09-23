2e7a7f66f9dd1781b243122fe5c70bf62581a060
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2020-09-22 21:10:46 -0400

    modify app heroku

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
+var porta = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
   return res.send("Learning Travis 2");
 })
 
 
-app.listen(4444);
+app.listen(porta);
