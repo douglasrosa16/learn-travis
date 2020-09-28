dc3f4845d3025648b69b586e1f12b4422d1acace
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2020-09-27 21:52:34 -0400

    add css

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-var porta = process.env.PORT || 8080;
+app.set("view engine","html");
+
+nunjucks.configure("views",{
+  express: app,
+  noCache: true
+});
+
+var port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  return res.send("Learning Travis 2");
-})
+  return res.render("home");
+});
 
 
-app.listen(porta);
+app.listen(port);
